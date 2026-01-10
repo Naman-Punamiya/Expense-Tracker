4824be3d9863e0394eae614a8ad1eec9d0c69b9b
Krishn <krishnkarelia@gmail.com>
2026-01-10 12:42:09 +0530

    Home page done

--- expenseTracker/urls.py
+++ expenseTracker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from expenseTracker.views import home
+# from views import home
 urlpatterns = [
+    path('',home,name="home"),
     path('admin/', admin.site.urls),
 ]
